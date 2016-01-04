4254649a7b9abc03c7227926ac18e3f2bb579b6a
weihetian <weihe@udel.edu>
2016-01-04 15:22:04 -0500

    front end done

---
+++ data_access/pull_pictures.php
@@ -0,0 +1,35 @@
+<?php
+$servername = "52.23.43.247";
+$username = "stak";
+$password = "stakstudios";
+$database = "gallery";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$database,"3306");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$offset=$_GET['offset'];
+
+$query = "SELECT * FROM picture ORDER BY id DESC LIMIT $offset,20 ";
+
+$image_array =array();
+
+$imageresult = mysqli_query($conn,$query);
+while($row = mysqli_fetch_array($imageresult)){
+  $image_id=$row['id'];
+  $image_url = $row['url'];
+  $image_description = $row['picture_description'];
+
+  $image_array[] = array("id"=>$image_id,"url"=>$image_url,"description"=>$image_description);
+}
+
+
+echo json_encode($image_array);
+mysqli_close($conn);
+
+
+
+?>
--- js/image_grid.js
+++ js/image_grid.js
@@ -1,7 +1,7 @@
 $( function() {
 
 
-   
+
   load();
 
 
@@ -17,7 +17,7 @@ $('.pic_enlarged_container').click(function(){
    $('.middle_layer').hide();
 })
 
-$(document).on('click', '.block', function(){ 
+$(document).on('click', '.block', function(){
   var url = $(this).attr('data-url');
   $('.middle_layer').show();
   $('.pic_enlarged_container').html("<img src='"+url+"'</img>");
@@ -25,30 +25,49 @@ $(document).on('click', '.block', function(){
 }
 )
 
- 
+
   var $container = $('.container').masonry({
     itemSelector: '.block',
     columnWidth: '.block'
   });
-   
+
+
+  var counter = 0;
+
   function load(){
-    var $items = getItems();
-    // hide by default
-    $items.hide();
-    // append to container
-    $container.append( $items );
-    $items.imagesLoaded().progress( function( imgLoad, image ) {
-      // get item
-      // image is imagesLoaded class, not <img>
-      // <img> is image.img
-      var $item = $( image.img ).parents('.block');
-      // un-hide item
-      $item.show();
-      // masonry does its thing
-      $container.masonry( 'appended', $item ).masonry();
+    $.getJSON('data_access/pull_pictures.php?offset='+counter, function(data) {
+
+      console.log(data);
+      var items = '';
+      for ( var i=0; i < data.length; i++ ) {
+
+        var item = '<div class="block" data-url="'+data[i].url+'">'+
+          '<div class="block_middle_layer"><p>'+data[i].description+'</p></div><img src="'+data[i].url+'" /></div>';
+
+        items += item;
+        counter++;
+      }
+
+
+      var $items =  $( items );
+      // hide by default
+      $items.hide();
+      // append to container
+      $container.append( $items );
+      $items.imagesLoaded().progress( function( imgLoad, image ) {
+        // get item
+        // image is imagesLoaded class, not <img>
+        // <img> is image.img
+        var $item = $( image.img ).parents('.block');
+        // un-hide item
+        $item.show();
+        // masonry does its thing
+        $container.masonry( 'appended', $item ).masonry();
+      });
+
     });
 
-    
+
   }
 
 
@@ -60,7 +79,7 @@ function randomInt( min, max ) {
 function getItem() {
   var width = 400;
   var height = randomInt( 200, 400 );
-  var imageurl = 'http://lorempixel.com/' + 
+  var imageurl = 'http://lorempixel.com/' +
       width + '/' + height + '/transport';
 
   var testurl = 'goos';
@@ -69,14 +88,29 @@ function getItem() {
   return item;
 }
 
-function getItems() {
-  var items = '';
-  for ( var i=0; i < 20; i++ ) {
-    items += getItem();
-  }
 
-  // return jQuery object
-  return $( items );
+//Get Items needs to be replaced by Json
+function getItems() {
+
+  $.getJSON('data_access/pull_pictures.php', function(data) {
+
+    console.log(data);
+    var items = '';
+    for ( var i=0; i < data.length; i++ ) {
+      var item = '<div class="block" data-url="'+data.url+'">'+
+        '<div class="block_middle_layer"><p>'+data.description+'</p></div><img src="'+data.url+'" /></div>';
+
+      items += item;
+    }
+    return $( items );
+  });
+  // var items = '';
+  // for ( var i=0; i < 20; i++ ) {
+  //   items += getItem();
+  // }
+  //
+  // // return jQuery object
+  // return $( items );
 }
 
 function enlarge(url){
